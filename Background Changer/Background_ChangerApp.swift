14bbf4904c156c54075ad0ad2456fb7bc870f551
Dylan Chidambaram <dylanchidambaram@gmail.com>
2025-01-31 12:08:55 -0800

    Initial Commit

---
+++ Background Changer/Background_ChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Background_ChangerApp.swift
+//  Background Changer
+//
+//  Created by Dylan Chidambaram on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct Background_ChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Background ChangerTests/Background_ChangerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Background_ChangerTests.swift
+//  Background ChangerTests
+//
+//  Created by Dylan Chidambaram on 1/31/25.
+//
+
+import Testing
+@testable import Background_Changer
+
+struct Background_ChangerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
